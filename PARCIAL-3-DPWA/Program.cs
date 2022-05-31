a64fc3ee2980c7a4285d1ba3b76c1039a06ca470
Raúl Peñate <raul.h.penate@gmail.com>
2022-05-30 23:20:45 -0600

    Adding CORS

--- PARCIAL-3-DPWA/Program.cs
+++ PARCIAL-3-DPWA/Program.cs
@@ -8,6 +8,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+// CORS
+builder.Services.AddCors();
+// Swagger
 builder.Services.AddSwaggerGen();
 
 //DI
@@ -27,6 +30,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// CORS
+app.UseCors(c => c.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 
 app.UseAuthorization();
 
